fb87168bbbb345dacb9eb3d303be0419d0ec4ef8
Melissa Ryan <melissaryandigital@gmail.com>
2020-11-17 13:24:46 -0600

    Displays messages from server, adds XSS sanitization, working on POST

--- client/scripts/messageView.js
+++ client/scripts/messageView.js
@@ -1,12 +1,93 @@
 var MessageView = {
-
-  render: _.template(`
-      <!--
-      <div class="chat">
-        <div class="username"></div>
-        <div></div>
-      </div>
-      -->
-    `)
-
+  render: _.template(`<div class="chat"><%= text %><div class="username"><%= username %></div><div></div></div>`)
 };
+
+$.getJSON(`http://parse.${window.CAMPUS}.hackreactor.com/chatterbox/classes/messages`,
+  function (data) {
+    let html = '';
+    let { results } = data;
+
+    let sanitizedClean = renderDAta(results);
+    console.log(sanitizedClean);
+    console.log(sanitizedClean);
+    for (let i = 0; i < sanitizedClean.length; i++) {
+      console.log(sanitizedClean[i]);
+      html += MessageView.render(results[i]);
+    }
+    $('#chats').append(html);
+  });
+
+
+String.prototype.escape = function () {
+  var tagsToReplace = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;'
+  };
+  return this.replace(/[&<>]/g, function (tag) {
+    return tagsToReplace[tag] || tag;
+  });
+};
+
+
+
+var renderDAta = function (results) {
+  let messageInfo = [];
+
+  results.forEach(obj => {
+    var renderedData = {};
+    var createdAt = (obj.createdAt).escape();
+    var objectId = (obj.objectId).escape();
+
+    if (obj.roomname === null) {
+      obj.roomname = '';
+      var roomname = obj.roomname;
+    } else {
+      var roomname = (obj.roomname).escape();
+    }
+
+    var text = (obj.text).escape();
+
+    if (obj.username === null) {
+      obj.username = '';
+      var username = obj.username;
+    }
+    var username = (obj.username).escape();
+
+    renderedData.createdAt = createdAt;
+    renderedData.objectId = objectId;
+    renderedData.roomname = roomname;
+    renderedData.text = text;
+    renderedData.username = username;
+
+    messageInfo.push(renderedData);
+  });
+
+  return messageInfo;
+};
+
+
+// window.onload = function () {
+//   renderDAta(DATA);
+// };
+
+/* <script>alert('hello')</script> */
+/* <img src="" onerror=alert(?!?!?!?!);> */
+// in the browser ' onerror= "alert('hello')"
+
+
+
+// createdAt: "2020-10-03T20:24:51.017Z"
+// objectId: "l50HCJ39mz"
+// roomname: "lobby"
+// text: "It's good to be the king"
+// updatedAt: "2020-10-03T20:24:51.017Z"
+// username: "Mel Brooks"
+
+// '<div class="chat">' +
+//   '<%= text %>' +
+// '<div class="username">' +
+//   '<%= username %>' +
+//   '</div>' +
+//   '<div>' + '</div>' +
+// '</div>'
--- client/scripts/messages.js
+++ client/scripts/messages.js
@@ -1,4 +1,54 @@
 var Messages = {
+  // createdAt: "2020-10-03T20:24:51.017Z"
+  // objectId: "l50HCJ39mz"
+  // updatedAt: "2020-10-03T20:24:51.017Z"
 
 
 };
+
+Messages.prototype.createNewMessage = function () {
+
+  // username: "Mel Brooks"
+  // roomname: "lobby"
+  // text: "It's good to be the king"
+
+  // Add these to object
+  // Return this message object?
+
+
+
+};
+
+
+// Sanitize object
+// Post object to server
+
+
+// createdAt: "2020-10-03T20:24:51.017Z"
+// objectId: "l50HCJ39mz" -SERVER SIDE?
+// roomname: "lobby"
+// text: "It's good to be the king"
+// updatedAt: "2020-10-03T20:24:51.017Z"
+// username: "Mel Brooks"
+
+
+// var message = {
+//   username: 'shawndrost',
+//   text: 'trololo',
+//   roomname: '4chan'
+// };
+
+// $.ajax({
+//   // This is the url you should use to communicate with the parse API server.
+//   url: 'http://parse.CAMPUS.hackreactor.com/chatterbox/classes/messages',
+//   type: 'POST',
+//   data: JSON.stringify(message),
+//   contentType: 'application/json',
+//   success: function (data) {
+//     console.log('chatterbox: Message sent');
+//   },
+//   error: function (data) {
+//     // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+//     console.error('chatterbox: Failed to send message', data);
+//   }
+// });
