bb0aa8657049589b30cc81a4a67dee6191deb2a6
Anthony Sim <anthonyysim@gmail.com>
2020-11-20 17:24:40 -0800

    almost finished, can't pass last test

--- client/scripts/friends.js
+++ client/scripts/friends.js
@@ -3,92 +3,34 @@ var Friends = {
   list: [],
 
   initialize: function () {
-
+    Friends.toggleStatus();
   },
 
   addFriend: function () {
+    $('#chats .chat').on('click', function () {
 
-    let username = document.querySelector('#chats');
-
-    username.addEventListener('click', function (event) {
-
-      Friends.list.push(event.target.innerText);
-      let user = event.target.innerText;
-      let friend = event.target;
-      Friends.toggleStatus(user);
+      Friends.toggleStatus(this.classList[1]);
     });
-
   },
-
-
-  // when we click a username, that user name will go to the friends list.
-  // it fires toggleStatus:
-
-
-  // toggleStatus:
-  // for loop around the Message.results and check to see if the username are in the friends list
-  // if it is on the friends list we add the friend class to that item
 
 
   toggleStatus: function (friend) {
 
-    //let $friend = friend;
+    let allUser = document.getElementsByClassName(friend);
 
-    let { results } = Messages;
-    for (let i = 0; i < results.length; i++) {
-      if (results[i]['username'] === friend) {
-        friend.classList.add('friend');
-        // let test = $('this').class('username');
-        console.log($friend);
-
-        //$('this').parent();
+    for (let i = 0; i < allUser.length; i++) {
+      if (!allUser[i].classList.contains('friend')) {
+        allUser[i].classList.add('friend');
       } else {
-        //console.log('no');
+        allUser[i].classList.remove('friend');
+
       }
     }
-
-
-
-    //let $friend = friend;
-
-    // let { results } = Messages;
-    // for (let i = 0; i < results.length; i++) {
-    //   if (Friends.list.includes(results[i]['username'])) {
-    //     friend.classList.add('friend');
-    //     console.log('yes');
-    //   }
-    // }
   }
+
+
+
 };
 
 
 
-// $user = $('.username');
-// //let chat = document.querySelector('.chat');
-
-// $user.on('click', function (event) {
-//   console.log(event.target.innerText);
-
-//   let username = event.target.innerText;
-//   let { results } = Messages;
-
-
-//   for (var i = 0; i < results.length; i++) {
-
-//     if (results[i]['username'] === username) {
-
-//       if (results[i]['status'] === true) {
-//         results[i]['status'] = false;
-//       } else {
-//         results[i]['status'] = true;
-//       }
-//     }
-//   }
-//   $(this).toggleClass('friend');
-// });
-// },
-
-// toggleFriendClass: function () {
-
-// }
-// }
